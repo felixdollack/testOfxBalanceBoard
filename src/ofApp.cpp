4e6d02f9b42e5b8da439bd358fc8f490409ebbb9
Felix Dollack <felix.dollack@googlemail.com>
2019-06-14 17:30:26 +0900

    Adapt visualization for multi-user

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -27,43 +27,24 @@ void ofApp::draw(){
     ofClear(0);
     ofSetColor(ofColor::royalBlue);
 
-    // draw individual sensors (corners) of wiiFit
-    ofDrawCircle(0.1 * screen_width, 0.1 * screen_height, 100 * this->balance_data.top_left);
-    ofDrawCircle(0.9 * screen_width, 0.1 * screen_height, 100 * this->balance_data.top_right);
-    ofDrawCircle(0.1 * screen_width, 0.9 * screen_height, 100 * this->balance_data.bottom_left);
-    ofDrawCircle(0.9 * screen_width, 0.9 * screen_height, 100 * this->balance_data.bottom_right);
-
-    // draw virtual X axis of wiiFit
-    float xpos = (0.1 * screen_width) + ((0.8 * screen_width) * this->balance_data.virtual_x);
     ofDrawLine(clearance,                clearance,
                screen_width - clearance, clearance);                 // x line top
     ofDrawLine(clearance,                screen_height - clearance,
                screen_width - clearance, screen_height - clearance); // x line bottom
-    ofDrawCircle(xpos, clearance,                 circle_size);      // circle top
-    ofDrawCircle(xpos, screen_height - clearance, circle_size);      // circle bottom
-    ofDrawLine(xpos, clearance,
-               xpos, screen_width - clearance);                      // intersection X
-
-    // draw virtual Y axis of wiiFit
-    float ypos = (0.9 * screen_height) - ((0.8 * screen_height) * this->balance_data.virtual_y);
     ofDrawLine(clearance, (0.1 * screen_height),
                clearance, (0.9 * screen_height));                    // y line left
     ofDrawLine(screen_width - clearance, (0.1 * screen_height),
                screen_width - clearance, (0.9 * screen_height));     // y line right
-    ofDrawCircle(clearance,                ypos, circle_size);       // circle left
-    ofDrawCircle(screen_width - clearance, ypos, circle_size);       // circle right
-    ofDrawLine(clearance, ypos,
-               screen_width - clearance, ypos);                      // intersection Y
 
-    // draw center of mass and weight
-    ofDrawCircle(xpos, ypos, 100 * this->balance_data.sum);
+    float xpos1 = (0.1 * screen_width) + ((0.8 * screen_width) * this->balance_data.virtual_x[0]);
+    float ypos1 = (0.1 * screen_height) + ((0.8 * screen_height) * this->balance_data.virtual_x[1]);
+    float xpos2 = (0.1 * screen_width) + ((0.8 * screen_width) * this->balance_data.virtual_x[2]);
+    float ypos2 = (0.9 * screen_height) - ((0.8 * screen_height) * this->balance_data.virtual_x[3]);
 
-    // draw absolute center
-    ofSetColor(ofColor::red);
-    ofDrawLine((screen_width/2) - clearance, (screen_height/2),
-               (screen_width/2) + clearance, (screen_height/2));
-    ofDrawLine((screen_width/2), (screen_height/2) - clearance,
-               (screen_width/2), (screen_height/2) + clearance);
+    ofDrawCircle(xpos1, clearance,                 circle_size);      // circle top
+    ofDrawCircle(clearance,                ypos1, circle_size);       // circle left
+    ofDrawCircle(xpos2, screen_height - clearance, circle_size);      // circle bottom
+    ofDrawCircle(screen_width - clearance, ypos2, circle_size);       // circle right
 }
 
 //--------------------------------------------------------------
